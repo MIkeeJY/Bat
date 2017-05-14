b832b70e8209ddb4110dae16101862db9b6a1acf
Little Von <122691411@qq.com>
2017-05-14 19:35:39 +0800

    更改hosts的批处理文件
    增加禁用常用搜索引擎和翻译网址的批处理

---
+++ Change_hosts/Change.bat
@@ -0,0 +1,28 @@
+@echo off
+c:
+cd %SystemRoot%\system32\drivers\etc\
+echo. >> hosts
+echo 0.0.0.0 cn.bing.com >> hosts
+echo 0.0.0.0 www.google.cn >> hosts
+echo 0.0.0.0 www.so.com >> hosts
+echo 0.0.0.0 www.sogou.com >> hosts
+echo 0.0.0.0 www.baidu.com >> hosts
+echo 0.0.0.0 baike.baidu.com >> hosts
+echo 0.0.0.0 zhidao.baidu.com >> hosts
+echo 0.0.0.0 www.hao123.com >> hosts
+echo 0.0.0.0 xueshu.baidu.com >> hosts
+echo 0.0.0.0 tieba.baidu.com >> hosts
+echo 0.0.0.0 blog.csdn.net >> hosts
+echo 0.0.0.0 www.zhihu.com >> hosts
+echo 0.0.0.0 github.com >> hosts
+echo 0.0.0.0 www.acmerblog.com >> hosts
+echo 0.0.0.0 www.youdao.com >> hosts
+echo 0.0.0.0 fanyi.baidu.com >> hosts
+echo 0.0.0.0 translate.google.cn >> hosts
+echo 0.0.0.0 fanyi.youdao.com >> hosts
+echo 0.0.0.0 www.iciba.com >> hosts
+echo 0.0.0.0 www.bing.com/translator >> hosts
+echo 0.0.0.0 dict.cn >> hosts
+echo 0.0.0.0 fy.iciba.com >> hosts
+echo 相关网页已被禁止...
+echo 请按回车继续...
